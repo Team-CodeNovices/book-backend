29a4e2850cfc682740bc116f243b9b036556b7cb
psh <sbag17230@gmail.com>
2024-03-14 21:53:50 +0900

    상세내용 페이지 수정

--- src/main/java/com/example/book/service/BookDetailInfo.java
+++ src/main/java/com/example/book/service/BookDetailInfo.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -16,9 +17,15 @@ public class BookDetailInfo {
     private final OurBookMapper book;
 
 
-    public List<OurBookDto> Bookdetailinfo(String bookName) {
-        return book.bookdetailinfo(bookName);
+    public List<OurBookDto> Bookdetailinfo(String bookname) {
+        if (bookname.length() < 2) {
+            // 키워드가 두 글자 미만이면 빈 리스트 반환
+            return new ArrayList<>();
+        } else {
+            // 두 글자 이상의 키워드가 포함된 책 정보 반환
+            return book.bookdetailinfo(bookname);
+        }
     }
-
-
 }
+
+
