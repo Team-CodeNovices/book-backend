83b4e62166ad31110cab5235937dc523db468ef3
psh <sbag17230@gmail.com>
2024-04-02 16:36:54 +0900

    독후감 페이지 수정본

--- src/main/java/com/example/book/controller/BookReportController.java
+++ src/main/java/com/example/book/controller/BookReportController.java
@@ -5,12 +5,9 @@ import com.example.book.service.BookReportService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import javax.servlet.http.HttpSession;
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -22,14 +19,46 @@ public class BookReportController {
 
     @ApiOperation("독후감 등록")
     @PostMapping("/registered")
-    public void reportInsert(@RequestParam String postimage, @RequestParam String bookname, @RequestParam String postconetents, HttpSession session) {
+    public void reportInsert(@RequestParam int useridx, @RequestParam String reporttitle, @RequestParam String postimage, @RequestParam String bookname, @RequestParam String postconetents, @RequestParam int userlike) {
         BookReportDto dto = new BookReportDto();
-//        int useridx = (int)session.getAttribute("userIdxs");
-//        dto.setUseridx(useridx);
+        dto.setUseridx(useridx);
+        dto.setReporttitle(reporttitle);
         dto.setPostimage(postimage);
         dto.setBookname(bookname);
         dto.setPostcontents(postconetents);
+        dto.setUserlike(userlike);
         service.reportInsert(dto);
+    }
+
+    @ApiOperation("독후감 전체보기")
+    @GetMapping("/reportall")
+    public List<BookReportDto> reportselect() {
+        return service.reportall();
+    }
+
+    @ApiOperation("독후감 상세보기")
+    @GetMapping("/reportselect")
+    public BookReportDto reportselect(BookReportDto dto){
+        return service.reportselect(dto);
+    }
+
+    @ApiOperation("독후감 수정")
+    @PutMapping("/update")
+    public int reportupdate(@RequestParam int postidx, @RequestParam String postcontents, @RequestParam String reporttitle) {
+        BookReportDto dto = new BookReportDto();
+        dto.setPostidx(postidx);
+        dto.setPostcontents(postcontents);
+        dto.setReporttitle(reporttitle);
+
+        return service.reportupdate(dto);
+    }
+    @ApiOperation("독후감 삭제")
+    @DeleteMapping("/delete")
+    public void reportdelete(@RequestParam  int postidx){
+        BookReportDto dto = new BookReportDto();
+        dto.setPostidx(postidx);
+
+        service.reportdelete(dto);
     }
 
 
--- src/main/java/com/example/book/service/BookReportService.java
+++ src/main/java/com/example/book/service/BookReportService.java
@@ -1,22 +1,35 @@
 package com.example.book.service;
 
-import com.example.book.dao.BookReport;
+import com.example.book.dao.BookReportMapper;
 import com.example.book.dto.BookReportDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class BookReportService {
 
-    private final BookReport dao;
+    private final BookReportMapper dao;
 
     public void reportInsert(BookReportDto dto) {
         dao.reportInsert(dto);
     }
 
+    public List<BookReportDto> reportall(){
+       return dao.reportall();
+    }
 
+    public BookReportDto reportselect(BookReportDto dto){
+        return dao.reportselect(dto);
 
+    }
+    public int  reportupdate(BookReportDto dto){
+        return dao.reportupdate(dto);
+    }
 
-
+    public void reportdelete(BookReportDto dto){
+        dao.reportdelete(dto);
+    }
 }
